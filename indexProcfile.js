2c1b567e3cc2e8fffee1ea37b464e4513caec152
Renan Oliveira <oliveira.renan.rg@gmail.com>
2021-07-26 09:09:46 -0300

    fix: env var

--- indexProcfile.js
+++ indexProcfile.js
@@ -1,14 +1,14 @@
-require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser').json();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+const MESSAGE = process.env.MESSAGE;
 
 app.use(bodyParser);
 
 app.get('/', (req, res) => {
-  res.status(200).send('Procfile existe mesmo!! Shouldn\'t matter but it does');
+  res.status(200).send(MESSAGE);
 })
 
 app.listen(PORT, () => console.log(`Escutando na porta ${PORT}`));
