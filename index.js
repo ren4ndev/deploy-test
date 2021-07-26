d1014f0f780572f13d78fd8b538a54ee95029235
Renan Oliveira <oliveira.renan.rg@gmail.com>
2021-07-26 08:58:12 -0300

    fix: ENV

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser').json();
 
 const PORT = process.env.PORT || 3000;
+const MESSAGE = process.env.MESSAGE;
 const app = express();
 
 app.use(bodyParser);
 
 app.get('/', (req, res) => {
-  res.status(200).send('Deu certinnnnn');
+  res.status(200).send(MESSAGE);
 })
 
 app.listen(PORT, () => console.log(`Escutando na porta ${PORT}`));
